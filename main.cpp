15979b22e3ea2730f818cc57e926c1bcdcaa82a0
isaiasm-hn <isaiasm_hn@unitec.edu>
2017-03-21 07:26:53 -0600

    Trabajando

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ using namespace std;
 vector<int> Baraja();
 vector<string> Baraja2();
 int readInt();
-string Repartir(vector<int>);
+int Carta(vector<int>);
 
 int main(){
 	vector<int> used_number;
-
+	cout<<Baraja().size();
 	return 0;
 }
 
@@ -41,71 +41,88 @@ int readInt() {
 	}while(!esNumero);
 
 }
-string Repartir(vector<int> used_numbers){
+int Repartir(vector<int> used_numbers){
+	bool retornar;
+	int retVal;
+	do{	
+		retornar=true;
+		retVal=(0 + rand() % 51);
+		for (int i = 0; i < used_numbers.size(); ++i){
+			if(retVal==used_number.at(i)){
+				retornar=false;
+			}
+		}
+		if(retornar){
+			return retVal;
+		}
 
+	}while(retornar);
 
 }
-vector<string> baraja2(){
-	vector<int> baraja;
-	baraja.push_back("2♠");
-	baraja.push_back("2♥");
-	baraja.push_back("2♦");
-	baraja.push_back("2♣");
+vector<string> Baraja2(){
+	vector<string> baraja;
 
-	baraja.push_back("3♠");
-	baraja.push_back("3♥");
-	baraja.push_back("3♦");
-	baraja.push_back("3♣");
+	baraja.push_back("2 ♠");
+	baraja.push_back("2 ♥");
+	baraja.push_back("2 ♦");
+	baraja.push_back("2 ♣");
 
-	baraja.push_back("4♠");
-	baraja.push_back("4♥");
-	baraja.push_back("4♦");
-	baraja.push_back("4♣");
+	baraja.push_back("3 ♠");
+	baraja.push_back("3 ♥");
+	baraja.push_back("3 ♦");
+	baraja.push_back("3 ♣");
 
-	baraja.push_back("5♠");
-	baraja.push_back("5♥");
-	baraja.push_back("5♦");
-	baraja.push_back("5♣");
+	baraja.push_back("4 ♠");
+	baraja.push_back("4 ♥");
+	baraja.push_back("4 ♦");
+	baraja.push_back("4 ♣");
 
-	baraja.push_back("6♠");
-	baraja.push_back("6♥");
-	baraja.push_back("6♦");
-	baraja.push_back("6♣");
+	baraja.push_back("5 ♠");
+	baraja.push_back("5 ♥");
+	baraja.push_back("5 ♦");
+	baraja.push_back("5 ♣");
 
-	baraja.push_back("7♠");
-	baraja.push_back("7♥");
-	baraja.push_back("7♦");
-	baraja.push_back("7♣");
+	baraja.push_back("6 ♠");
+	baraja.push_back("6 ♥");
+	baraja.push_back("6 ♦");
+	baraja.push_back("6 ♣");
 
-	baraja.push_back("8♠");
-	baraja.push_back("8♥");
-	baraja.push_back("8♦");
-	baraja.push_back("8♣");
+	baraja.push_back("7 ♠");
+	baraja.push_back("7 ♥");
+	baraja.push_back("7 ♦");
+	baraja.push_back("7 ♣");
 
-	baraja.push_back("9♠");
-	baraja.push_back("9♥");
-	baraja.push_back("9♦");
-	baraja.push_back("9♣");
+	baraja.push_back("8 ♠");
+	baraja.push_back("8 ♥");
+	baraja.push_back("8 ♦");
+	baraja.push_back("8 ♣");
 
-	baraja.push_back("10♠");
-	baraja.push_back("10♥");
-	baraja.push_back("10♦");
-	baraja.push_back("10♣");
+	baraja.push_back("9 ♠");
+	baraja.push_back("9 ♥");
+	baraja.push_back("9 ♦");
+	baraja.push_back("9 ♣");
 
-	baraja.push_back("J♠");
-	baraja.push_back("♥");
-	baraja.push_back("♦");
-	baraja.push_back("♣");
+	baraja.push_back("10 ♠");
+	baraja.push_back("10 ♥");
+	baraja.push_back("10 ♦");
+	baraja.push_back("10 ♣");
 
-	baraja.push_back("♠");
-	baraja.push_back("♥");
-	baraja.push_back("♦");
-	baraja.push_back("♣");
+	baraja.push_back("J ♠");
+	baraja.push_back("J ♥");
+	baraja.push_back("J ♦");
+	baraja.push_back("J ♣");
 
-	baraja.push_back("♠");
-	baraja.push_back("♥");
-	baraja.push_back("♦");
-	baraja.push_back("♣");
+	baraja.push_back("Q ♠");
+	baraja.push_back("Q ♥");
+	baraja.push_back("Q ♦");
+	baraja.push_back("Q ♣");
+
+	baraja.push_back("A ♠");
+	baraja.push_back("A ♥");
+	baraja.push_back("A ♦");
+	baraja.push_back("A ♣");
+	
+	return baraja;
 
 }
 
@@ -115,49 +132,65 @@ vector<int> Baraja(){
 	baraja.push_back(2);
 	baraja.push_back(2);
 	baraja.push_back(2);
+
 	baraja.push_back(3);
 	baraja.push_back(3);
 	baraja.push_back(3);
 	baraja.push_back(3);
+
 	baraja.push_back(4);
 	baraja.push_back(4);
 	baraja.push_back(4);
 	baraja.push_back(4);
+
 	baraja.push_back(5);
 	baraja.push_back(5);
 	baraja.push_back(5);
 	baraja.push_back(5);
+
 	baraja.push_back(6);
 	baraja.push_back(6);
 	baraja.push_back(6);
 	baraja.push_back(6);
+
 	baraja.push_back(7);
 	baraja.push_back(7);
 	baraja.push_back(7);
 	baraja.push_back(7);
+
 	baraja.push_back(8);
 	baraja.push_back(8);
 	baraja.push_back(8);
 	baraja.push_back(8);
+
 	baraja.push_back(9);
 	baraja.push_back(9);
 	baraja.push_back(9);
+	baraja.push_back(9);
+
 	baraja.push_back(10);
 	baraja.push_back(10);
 	baraja.push_back(10);
 	baraja.push_back(10);
+
+
 	baraja.push_back(10);
 	baraja.push_back(10);
 	baraja.push_back(10);
 	baraja.push_back(10);
+
+	
 	baraja.push_back(10);
 	baraja.push_back(10);
 	baraja.push_back(10);
 	baraja.push_back(10);
+
 	baraja.push_back(10);
 	baraja.push_back(10);
 	baraja.push_back(10);
 	baraja.push_back(10);
+	
+
 	baraja.push_back(11);
 	baraja.push_back(11);
 	baraja.push_back(11);
